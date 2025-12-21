0ae74aeeca314e0e9db1f43ff1cd42cf2f0903f2
hanumankind1 <johanreads@gmail.com>
2025-12-21 13:30:04 -0500

    updated compileSdk value to 36

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/auto/pedroPathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/auto/pedroPathing/Tuning.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.teamcode.auto.auto.pedroPathing;
 
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.changes;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.draw;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.drawOnlyCurrent;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.follower;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.stopRobot;
-import static org.firstinspires.ftc.teamcode.pedroPathing.Tuning.telemetryM;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.changes;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.draw;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.drawOnlyCurrent;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.follower;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.stopRobot;
+import static org.firstinspires.ftc.teamcode.auto.auto.pedroPathing.Tuning.telemetryM;
 
 import com.bylazar.configurables.PanelsConfigurables;
 import com.bylazar.configurables.annotations.Configurable;
