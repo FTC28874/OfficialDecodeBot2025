2257787c59d65c9e5d03a472ead9dcde6a862ca8
hanumankind1 <johanreads@gmail.com>
2026-01-25 13:20:35 -0500

    cleaned up XY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tests/TestShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/tests/TestShooter.java
@@ -1,42 +1,27 @@
 package org.firstinspires.ftc.teamcode.teleop.tests;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-import org.firstinspires.ftc.teamcode.robot.Intake;
 import org.firstinspires.ftc.teamcode.PIDtoPoint.drive.GoBildaPinpointDriver;
 import org.firstinspires.ftc.teamcode.robot.dynamicShooter;
 
 import org.firstinspires.ftc.teamcode.robot.Shooter;
 
-@TeleOp(name="Test Shooter 123", group="Linear OpMode")
+@TeleOp(name="Test XY", group="Linear OpMode")
 
 public class TestShooter extends LinearOpMode {
-
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor shooterU = null;
-    private DcMotor shooterD = null;
-    private DcMotor intake = null;
     private GoBildaPinpointDriver odo = null;
-    Pose2D pos = null; //odo.getPosition(); ✅ ️
-
-
-
+    Pose2D pos = null;
 
     @Override
     public void runOpMode() {
         odo = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
-        odo.setOffsets(-48, -156, DistanceUnit.INCH); //these are tuned for 3110-0002-0001 Product Insight #1
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
-
 
         odo.resetPosAndIMU();
 
@@ -44,13 +29,9 @@ public class TestShooter extends LinearOpMode {
 
         waitForStart();
         while (opModeIsActive()) {
-
-//            shooterU.setPower(gamepad1.right_stick_y);
-//            shooterD.setPower(gamepad1.right_stick_y);
             odo.update();
             pos = odo.getPosition();
             double shooterEncSpeed = dynamicShooter.flywheelSpeed(dynamicShooter.distanceFromGoal(pos.getX(DistanceUnit.INCH), pos.getY(DistanceUnit.INCH), pos.getHeading(AngleUnit.RADIANS)));
-//            Shooter.setShooterPower();
             telemetry.addData("Robot X: ", pos.getX(DistanceUnit.INCH));
             telemetry.addData("Robot Y: ", pos.getY(DistanceUnit.INCH));
             telemetry.addData("Shooter Power: ", gamepad1.right_stick_y);
