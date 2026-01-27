7955126ec2dce94b107a7b8b1272ceafacf439db
hanumankind1 <johanreads@gmail.com>
2026-01-27 14:52:18 -0500

    equations for dynamic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Helper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Helper.java
@@ -10,5 +10,24 @@ public class Helper {
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
     }
 
-    public static double calculateRPM(double x) { return 0.000119897 * Math.pow(x, 4) - 0.0516745 * Math.pow(x, 3) + 7.98281 * Math.pow(x, 2) - 506.0992 * x + 13201.3049; }
+    public static double calcTargetRPM(double dist) {
+        double rpm = -0.0150214 * Math.pow(dist, 2) + 11.90772 * dist + 1365.70845;
+        rpm = Math.min(Constants.MAX_SHOOTER_RPM, Math.max(Constants.MIN_SHOOTER_RPM, rpm));
+        return rpm;
+    }
+
+    public static double calcHoodPos(double dist) {
+        // Equation: y = -0.000110842x^2 + 0.0205728x - 0.360103
+        double hoodPos = -0.000110842 * Math.pow(dist, 2) + 0.0205728 * dist - 0.360103;
+        hoodPos =  Math.min(Constants.MAX_HOOD_POSITION, Math.max(Constants.MIN_HOOD_POSITION, hoodPos));
+        return hoodPos;
+    }
+
+    public static double calcTurretHead(double robotHeading) {
+        // Equation: y = 0.0450189x^2 + 0.286092x - 34.64081
+        double turHead = 0.0450189 * Math.pow(robotHeading, 2) + 0.286092 * robotHeading - 34.64081;
+        turHead = Math.min(Constants.MAX_TURRET_HEAD, Math.max(Constants.MIN_TURRET_HEAD, turHead));
+        return turHead;
+    }
+
 }
