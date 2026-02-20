a035cac0a85aacd24937f24441865e176c9d5721
Johan J <johanreads@gmail.com>
2026-02-20 16:34:24 -0500

    teleops now under group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RedTeleOp.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.turret.RedGoalCentering;
  * RedTeleOp — RED alliance (AprilTag ID 24).
  * All robot logic inherited from NewTeleOp.
  */
-@TeleOp(name = "Red TeleOp", group = "Camera TeleOp")
+@TeleOp(name = "Red Alliance", group = "Camera TeleOp")
 public class RedTeleOp extends NewTeleOp {
     @Override
     protected AprilTagCenteringBase createCenteringSubsystem() {
