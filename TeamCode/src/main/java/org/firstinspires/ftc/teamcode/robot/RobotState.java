1b3f5af84006ca55c9fcd6dd2d4b2f784f5b02b3
Hrishaan1 <hrishaan.neelam@icloud.com>
2026-02-21 18:14:54 -0500

    made robotstate variables just init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotState.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.robot;
 
 public class RobotState {
-    public static double savedX = 0;
-    public static double savedY = 0;
-    public static double savedHeading = 0;
+    public static double savedX;
+    public static double savedY;
+    public static double savedHeading;
 }
